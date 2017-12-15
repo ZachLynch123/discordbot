094478edccbc576fcab9b6b20f7b123b04bc9cf6
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-15 07:36:18 -0800

    Bot works

--- bot.py
+++ bot.py
@@ -1,7 +1,27 @@
 import discord
 import asyncio
 import os
+from discord.ext.commands import Bot
+from discord.ext import commands
+import platform
+
+# Constant
+client_token = os.environ.get('DISCORD_TOKEN')
+
+client = Bot(description='Hello!', command_prefix='?', pm_help=True)
+def main(): 
+
+	client = Bot(description='Hey all! Im the best!', command_prefix='?', pm_help=True)
+
+	@client.event
+
+	async def on_ready(): 
+		print('Github link: https://github.com/ZachLynch123/discordbot')
 
 
-client_id = os.environ.get('DISCORD_ID')
-print(client_id)
+
+
+
+if __name__ == '__main__':
+	client.run(client_token)
+
