8a7772e63144fb7d58081e668834e2bd060a7229
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-18 18:50:41 -0800

    Added Music_stuff.py to handle all music commands for bot. Also added more dependencies for requirements.txt

--- Music_stuff.py
+++ Music_stuff.py
@@ -1,31 +1,60 @@
-import discord
 import asyncio
+import discord
 import os
-from discord.ext.commands import Bot
 from discord.ext import commands
-import platform
-import random
-from imgurpython import ImgurClient
-import pdb
 
-# Constant
 client_token = os.environ.get('DISCORD_TOKEN')
-imgur_id = os.environ.get('IMGUR_ID')
-imgur_secret = os.environ.get('IMGUR_SECRET')
-imgur = ImgurClient(imgur_id, imgur_secret)
-# Set's the bot's description and prexix to any command
-client = Bot(description='Hello!', command_prefix='-', pm_help=False)
-# main function that handles commands
-@client.event
-async def on_ready():
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('-------')
 
-@client.command(pass_context=True)
-async def ping(ctx): 
-	await client.say("pong!")
+
+class VoiceEntry:
+    def __init__(self, message, player):
+        self.requester = message.author
+        self.channel = message.channel
+        self.player = player
+
+    def __str__(self):
+        fmt = '*{0.title}* uploaded by {0.uploader} and requested by {1.display_name}'
+        duration = self.player.duration
+        if duration:
+            fmt = fmt + ' [length: {0[0]}m {0[1]}s]'.format(divmod(duration, 60))
+        return fmt.format(self.player, self.requester)
+
+class VoiceState:
+    def __init__(self, bot):
+        self.current = None
+        self.voice = None
+        self.bot = bot
+        self.play_next_song = asyncio.Event()
+        self.songs = asyncio.Queue()
+        self.skip_votes = set() # a set of user_ids that voted
+        self.audio_player = self.bot.loop.create_task(self.audio_player_task())
+
+    def is_playing(self):
+        if self.voice is None or self.current is None:
+            return False
+
+        player = self.current.player
+        return not player.is_done()
+
+    @property
+    def player(self):
+        return self.current.player
+
+    def skip(self):
+        self.skip_votes.clear()
+        if self.is_playing():
+            self.player.stop()
+
+    def toggle_next(self):
+        self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
+
+    async def audio_player_task(self):
+        while True:
+            self.play_next_song.clear()
+            self.current = await self.songs.get()
+            await self.bot.send_message(self.current.channel, 'Now playing ' + str(self.current))
+            self.current.player.start()
+            await self.play_next_song.wait()
 
 class Music:
     """Voice related commands.
@@ -198,21 +227,11 @@ class Music:
             skip_count = len(state.skip_votes)
             await self.bot.say('Now playing {} [skips: {}/3]'.format(state.current, skip_count))
 
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('$'), description='A playlist example for discord.py')
+bot.add_cog(Music(bot))
 
+@bot.event
+async def on_ready():
+    print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
 
-
-
-
-#async def random(): 
-#	url_list = []
-#	i = 0
-#	items = imgur.gallery()
-#	for item in items: 
-#		url_list.append(item.link)
-#		i = i + 1
-#
-#	imglink = random.choice(url_list)
-#	await client.say((random.choice(url_list)))
-
-client.run(client_token)
-
+bot.run(client_token)
--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-x = '-search this'
-x = x.split(' ', 1)[1]
+from imgurpython import ImgurClient
+import os
+import random
+client_token = os.environ.get('DISCORD_TOKEN')
+imgur_id = os.environ.get('IMGUR_ID')
+imgur_secret = os.environ.get('IMGUR_SECRET')
+client = ImgurClient(imgur_id, imgur_secret)
+i = 0
+url_list = []
+items = client.gallery()
+for item in items:
+		url_list.append(item.link) 
+		i = i + 1
+x = random.choice(url_list)
 print(x)
