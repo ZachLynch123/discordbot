e4498e61c022131393994ca541391c00ae4d95d2
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-26 19:04:13 -0800

    edited ignore file and added stuff to music_stuff.py

--- query.py
+++ query.py
@@ -1,1 +1,12 @@
-query.py
+import random
+
+def query(search): 
+	url_list = []
+	items = client.gallery_search(search) # Searches Imgur for all the items with the query
+	i = 0
+	for item in items: 
+		url_list.append(item.link)
+		i = i + 1
+	img = random.choice(url_list)
+	return img
+	
